1962baef7414e534170c9aa475a39f4141c3ae3b
zhangxj_a_sc <zhangxj_a_sc@si-tech.com.cn>
2024-07-04 21:21:55 +0800

    build(init): 项目初始化

---
+++ crm-om-web/src/test/java/crm/om/OmWebApplicationTests.java
@@ -0,0 +1,13 @@
+package crm.om;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
